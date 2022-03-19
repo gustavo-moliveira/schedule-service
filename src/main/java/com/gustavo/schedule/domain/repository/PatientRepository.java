e4cce21b456d566d5281217d5e89620417bae4b3
gustavo-moliveira <gustavo.moliveira@bandtec.com.br>
2022-03-19 03:44:50 -0300

    cpf validation and email validation

--- src/main/java/com/gustavo/schedule/domain/repository/PatientRepository.java
+++ src/main/java/com/gustavo/schedule/domain/repository/PatientRepository.java
@@ -2,9 +2,14 @@ package com.gustavo.schedule.domain.repository;
 
 import com.gustavo.schedule.domain.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+    Optional<Patient> findByCpf(String cpf);
+    Optional<Patient> findByEmail(String email);
 }
