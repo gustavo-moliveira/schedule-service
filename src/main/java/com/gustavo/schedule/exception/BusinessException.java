0ae17a5aa5b070809d13a50507dc1669014b96c6
gustavo-moliveira <gustavo.moliveira@bandtec.com.br>
2022-03-19 03:43:09 -0300

    BusinessException creation

---
+++ src/main/java/com/gustavo/schedule/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.gustavo.schedule.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+}
