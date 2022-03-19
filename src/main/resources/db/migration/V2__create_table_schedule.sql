9e40950e8b994a3a147d3c15cf60fa1507b3d09b
gustavo-moliveira <gustavo.moliveira@bandtec.com.br>
2022-03-19 02:13:36 -0300

    schedule table creation

---
+++ src/main/resources/db/migration/V2__create_table_schedule.sql
@@ -0,0 +1,8 @@
+CREATE TABLE schedule(
+	id serial PRIMARY KEY,
+	description varchar(255),
+	date_time timestamp,
+	creation_date timestamp,
+	patient_id integer,
+	CONSTRAINT fk_schedule_patieint FOREIGN KEY(patient_id) REFERENCES patient(id)
+);
