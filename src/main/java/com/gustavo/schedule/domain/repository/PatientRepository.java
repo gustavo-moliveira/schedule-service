cad2fe484cf01a55f35d4432ee2ff06678c24050
gustavo-moliveira <gustavo.moliveira@bandtec.com.br>
2022-03-19 02:15:38 -0300

    repository creation

---
+++ src/main/java/com/gustavo/schedule/domain/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.gustavo.schedule.domain.repository;
+
+import com.gustavo.schedule.domain.entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+}
